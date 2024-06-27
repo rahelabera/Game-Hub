81759c1617212e1327b4a8a7e86017d18942ef80
Rahel Abera <rahel.aberare@gmail.com>
2024-06-27 14:25:09 +0300

    Create a generic data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 function App() {
   return (
     <Grid templateAreas={{base: `"nav""main"`, lg: `"nav nav""aside main"`}}>
@@ -9,7 +10,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" >
-          Aside
+          <GenreList/>
         </GridItem>
       </Show>
       <GridItem area="main ">
