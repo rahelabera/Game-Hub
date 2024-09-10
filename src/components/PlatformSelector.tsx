9f7b27bf8c202956c70842e50116f5902e5b6784
Rahel Abera <rahel.aberare@gmail.com>
2024-09-10 10:20:15 +0300

    Build search input

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -18,7 +18,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
       </MenuButton>
       <MenuList> 
         {data.map((platform) => (
-          <MenuItem onClick={() => onSelectPlatform(platform)}>
+          <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>
             {platform.name}
           </MenuItem>
         ))}
