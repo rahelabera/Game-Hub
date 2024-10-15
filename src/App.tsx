e0ea1c6d28a3718e1e0aa9516f97b3ef41b4252a
Rahel Abera <rahel.aberare@gmail.com>
2024-10-15 10:41:34 +0300

    Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
       <GridItem area="main ">
         <Box paddingLeft={2}>
           <GameHeading gameQuery={gameQuery} />
-          <Flex  marginBottom={5} style={{ gap: '16px' }}>
+          <Flex  marginBottom={5}>
             <PlatformSelector
               selectedPlatform={gameQuery.platform}
               onSelectPlatform={(platform) =>
